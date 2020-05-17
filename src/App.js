10f92a82afec48593b8b17825a3767e951d96b83
Donghoon <donghoon@beautyops.co.kr>
2020-05-17 13:17:04 +0900

    prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { forwardRef, useRef, useState }from 'react';
+import { Button, Checkbox as Cb } from '@material-ui/core/';
 
-function App() {
+import audios from './audio';
+
+const CheckBox = forwardRef((_, audioRef) => {
+  const [checked, setChecked] = useState(true);
+  const onChange = () => {
+    if (checked) {
+      audioRef.current.volume = 0;
+      setChecked(false);
+    } else {
+      audioRef.current.volume = 1;
+      setChecked(true);
+    }
+  }
+  return <Cb checked={checked} onChange={onChange} />
+});
+
+const PlayButton = forwardRef((_, refs) => {
+  const [isPlaying, setisPlaying] = useState(false);
+  const label = isPlaying ? 'Pause' : 'Play';
+  const onClick = () => {
+    if (isPlaying) {
+      refs.forEach((ref) => ref.current.pause());
+    } else {
+      refs.forEach((ref) => ref.current.play());
+    }
+    setisPlaying(!isPlaying)
+  };
+
+  return <Button onClick={onClick} variant="contained">{label}</Button>;
+});
+
+const App = () => {
+  const refs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+  const baseRef = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>Test</p>
+      {Object.values(audios).map((audio, idx) => <audio key={audio} ref={refs[idx]} src={audio}/>)}
+      <audio ref={baseRef} src={audios.e} />
+      {refs.map((ref, idx) => <CheckBox key={idx} ref={ref}/>)}
+      <PlayButton ref={[...refs, baseRef]}/>
+    </>
   );
 }
 
---
+++ src/audio/index.js
@@ -0,0 +1,13 @@
+import a from './1.ogg';
+import b from './2.ogg';
+import c from './3.ogg';
+import d from './4.ogg';
+import e from './5.ogg';
+
+export default {
+  a,
+  b,
+  c,
+  d,
+  e,
+};
