f97873ea24d1013aa67b6894cb7bf26734b7cba1
Donghoon <donghoon@beautyops.co.kr>
2020-05-30 01:08:09 +0900

    add Slider bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext, useState }from 'react';
-import { Button, Checkbox } from '@material-ui/core';
+import { Box, Button, Checkbox, Slider } from '@material-ui/core';
 
 import audios from './audio';
 
@@ -26,11 +26,27 @@ const AudioProvider = ({ children }) => {
   );
 };
 
+const AudioSlider = () => {
+  const { refs } = useAudio();
+  const [sliderValue, setSliderValue] = useState(0);
+  const sliderUnit = 373 / 100;
+  const onChange = (_, value) => {
+    const newCurrentTime = value * sliderUnit;
+    Object.values(refs).forEach((ref) => {
+      ref.current.currentTime = newCurrentTime;
+    });
+    setSliderValue(value)
+  };
+
+  return <Slider value={sliderValue} onChange={onChange} />;
+};
+
 const AudioVolume = ({ src }) => {
   const { refs } = useAudio();
   const audioRef = refs[src];
   const volume = !!audioRef.current?.volume;
   const [checked, setChecked] = useState(volume);
+  console.log(refs?.f1?.current?.currentTime);
   useEffect(() => setChecked(volume), [volume]);
   const toggleVolume = () => {
     audioRef.current.volume = checked ? 0 : 1;
@@ -43,7 +59,7 @@ const AudioVolume = ({ src }) => {
 
 const PlayButton = () => {
   const { isPlaying, refs, setIsPlaying } = useAudio();
-  const { piano: baseRef } = refs;
+  console.log(refs);
   const label = isPlaying ? 'Pause' : 'Play';
   const onClick = () => {
     if (isPlaying) {
@@ -52,7 +68,7 @@ const PlayButton = () => {
         ref.current.pause();
       });
     } else {
-      const { current: { currentTime } } = baseRef;
+      const { current: { currentTime } } = refs.f1;
       Object.values(refs).forEach((ref) => {
         ref.current.volume = 1;
         ref.current.currentTime = currentTime;
@@ -69,9 +85,12 @@ const App = () => (
   <AudioProvider>
     <p>Test</p>
     <AudioContext.Consumer>
-      {(value) => Object.keys(value.refs).slice(0, -1).map((key) => <AudioVolume key={key} src={key} />)}
+      {(value) => Object.keys(value.refs).map((key) => <AudioVolume key={key} src={key} />)}
     </AudioContext.Consumer>
     <PlayButton />
+    <Box width={500}>
+      <AudioSlider />
+    </Box>
   </AudioProvider>
 );
 
